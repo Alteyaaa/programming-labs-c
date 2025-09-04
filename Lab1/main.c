4d77435ba16b0bf380240e172c8677467cfd590b
Lichagina Kristina <lichagina.kristina07@gmail.com>
2025-09-05 01:07:03 +0300

    reduce code

--- Lab1/main.c
+++ Lab1/main.c
@@ -13,31 +13,27 @@ int main() {
     asm(
         /* (C * E) - D */
         "movl %1, %%eax;"       /* C -> EAX */
-        "movl %2, %%ebx;"       /* E -> EBX */
-        "imull %%ebx;"          /* EAX * EBX -> EAX */
-        "movl %3, %%ebx;"       /* D -> EBX*/
-        "subl %%ebx, %%eax;"    /* EAX - EBX -> EAX */
+        "imull %2;"             /* EAX * E -> EAX */
+        "subl %3, %%eax;"       /* EAX - D -> EAX */
 
         /* term1 = B * (C * E – D) / C */
-        "movl %4, %%ebx;"       /* B -> EBX */
-        "imull %%ebx;"          /* EAX * EBX -> EAX */
-        "movl %1, %%ebx;"       /* C -> EBX */
-        "idivl %%ebx;"          /* EAX / EBX -> EAX */
+        "imull %4;"             /* EAX * B -> EAX */
+        "cdq;"                  /* расширяем EAX до EDX:EAX для деления */
+        "idivl %1;"             /* EDX:EAX / C -> EAX */
         "movl %%eax, %%ecx;"    /* EAX -> ECX */
 
         /* term2 = A * (F / D) */
         "movl %5, %%eax;"       /* F -> EAX */
-        "movl %3, %%ebx;"       /* D -> EBX */
-        "idivl %%ebx;"          /* EAX / EBX -> EAX*/
-        "movl %6, %%ebx;"       /* A -> ABX */
-        "imull %%ebx;"          /* EBX * EAX -> EAX */
+        "cdq;"                  /* расширяем EAX до EDX:EAX для правильного деления */
+        "idivl %3;"             /* EDX:EAX / D -> EAX*/
+        "imull %6;"             /* A * EAX -> EAX */
 
         /* res1 = term1 + term2 */
         "addl %%ecx, %%eax;"    /* ECX + EAX -> EAX */
         "movl %%eax, %0;"       /* EAX -> res1 */
         : "=r" (res1)
         : "r" (C), "r" (E), "r" (D), "r" (B), "r" (F), "r" (A)
-        : "%eax", "%ebx", "%ecx", "edx"
+        : "%eax", "%ecx", "edx"
         );
 
     printf("'ASM' result: %d * (%d * %d - %d) / %d + %d * (%d / %d) = %d\n", B, C, E, D, C, A, F, D, res1);
@@ -45,6 +41,9 @@ int main() {
     res2 = B * (C * E - D) / C + A * (F / D);
     printf("'C' result: %d * (%d * %d - %d) / %d + %d * (%d / %d) = %d", B, C, E, D, C, A, F, D, res2);
 
+    // 1 7 5 7 2 4 проверка рез-тов
+    // 10 2 3 4 2 1
 
     return 0;
 }
+
