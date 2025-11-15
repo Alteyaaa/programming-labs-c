4764100e714ac603df49ea51914057a60cbafdfa
Lichagina Kristina <lichagina.kristina07@gmail.com>
2025-11-15 11:26:20 +0300

    Lab 11 (started)

---
+++ Lab11.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+typedef struct {
+    float x;
+    float y;
+    float radius;
+} Circle;
+
+Circle createCircle(float x, float y, float r) {
+    Circle circle;
+    str
+}
+
+
+
+int main() {
+
+}
